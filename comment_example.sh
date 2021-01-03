a1db48f62b2b704b1d628e182cf661f2a4789fbe
sathya24 <ssnsathi125@gmail.com>
2021-01-03 11:30:36 +0530

    basic command shell

---
+++ comment_example.sh
@@ -0,0 +1,6 @@
+for (( counter=15; counter>1; counter-- ))
+do
+echo -n  "$counter "
+done
+printf "\n"
+
---
+++ shell_basics/first.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sathya1="what is your name"
+sathya2="PREETHI"
+echo $sathya1 
+echo $sathya2
+
+echo "what is your age"
+read AGE
+
+echo "My age is"$AGE
