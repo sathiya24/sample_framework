d3860fc31d72c807c015e58ac4657d408ec5f7d9
sathiya24 <you@example.com>
2021-01-18 18:33:04 +0530

    added shell script

---
+++ w_gitsync.sh
@@ -0,0 +1,16 @@
+echo "Git status command is shown in below command"
+git status
+echo "Difference in files are shown in below command"
+ls
+
+
+echo "yes both are same"
+
+echo "no they are noty same"
+
+if (x==='5')
+  echo "this is 5"
+else 
+    echo "this is something else"
+
+
