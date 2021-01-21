de1a13f6b45c8bfe276f23027073107c5b1f617f
sathiya24 <ssnsathi125@gmail.com>
2021-01-21 11:38:19 +0530

    n1

--- z-task-final.sh
+++ z-task-final.sh
@@ -1,13 +1,32 @@
 #!/bin/sh
 
-x=`git rev-parse --abbrev-ref HEAD`
-git checkout main
-git pull
-count=`git rev-list --count $x..main`
-echo $x $count
-if [ $count > 0 ] 
-then
-    echo "not upto date"
+#x=`git rev-parse --abbrev-ref HEAD`
+#git checkout main
+#it pull
+#count=`git rev-list --count $x..main`
+#echo $x $count
+#if [ $count > 0 ] 
+#then
+ #   echo "upto date"
+#lse
+ #   echo "not upto date"
+#fi
+NAME="Bob"
+
+#if [ "$NAME" == "Brad" ]
+#then
+#echo "your name is Brad"
+#elif [ "$NAME" == "jack" ]
+#then 
+#echo "your name is jack"
+#fi
+
+
+#FILE CONDITIONS
+FILE="test.txt"
+if [ -e "$FILE" ]
+then 
+echo "$FILE exists"
 else
-    echo "upto date"
+echo "$FILE does not exists"
 fi
