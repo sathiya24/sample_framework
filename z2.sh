fa08fa4dea06d05c4dbf49cf33085ac2de744e0a
sathiya24 <76838150+sathiya24@users.noreply.github.com>
2021-01-21 18:50:19 +0530

    Test branch (#2)
    * new change
    
    * new 1
    
    * 1234
    
    * 123

---
+++ z2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+my_branch=`git rev-parse --abbrev-ref HEAD`
+git checkout main
+git pull 
+count=`git rev-list --count $my_branch..main`
+echo $my_branch $count
+if [ $count -gt 0 ] 
+then 
+   echo " not upto date"
+else
+   echo "upto date"
+
+fi
+
