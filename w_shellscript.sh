264ca0ac2873f49a0332622c95aa22096f3fcff7
sathiya24 <ssnsathi125@gmail.com>
2021-01-22 14:21:48 +0530

    nc

---
+++ w_shellscript.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+my_branch=`git rev-parse --abbrev-ref HEAD`
+git checkout main
+git pull 
+count=`git rev-list --count $my_branch..main`
+echo $my_branch $count
+if [ $count -gt 0 ] 
+then 
+   echo "upto date"
+else
+   echo "not upto date"
+
+fi
+
