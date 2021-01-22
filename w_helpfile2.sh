20f8550d84fcdc0b782cf28e05765aa6114257ff
sathiya24 <ssnsathi125@gmail.com>
2021-01-22 14:19:34 +0530

    21

---
+++ w_helpfile2.sh
@@ -0,0 +1,40 @@
+functions
+sathya(){
+    echo "Hello $3 $2 "
+    return 10
+}
+
+sathya vik var vai aru
+
+ret=$?
+echo "return value is $ret"
+
+number_one () {
+    echo "Alpha calling .....over"
+    number_two
+}
+number_two(){
+    echo "Beta calling......over"
+}
+
+
+number_one
+
+
+
+number_one () {
+   echo "This is the first function speaking..."
+   number_two
+}
+
+number_two () {
+   echo "This is now the second function speaking..."
+   return 20
+}
+
+number_one
+ret=$?
+echo "return value is $ret"
+
+
+$man command
