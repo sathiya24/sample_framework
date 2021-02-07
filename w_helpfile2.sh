f54f5f3fa38b2e3030fc0350f25c4e64c4f84c3c
sathiya24 <ssnsathi125@gmail.com>
2021-02-07 21:26:14 +0530

    cvb

--- w_helpfile2.sh
+++ w_helpfile2.sh
@@ -1,26 +1,26 @@
 #functions
-Hello(){
+Hello() {
     echo "hello world $3 $2"
     return 20
 }
 
-Hello 
+Hello
 
 ret=$?
 
 echo "returning to return value $ret"
 
-Hello(){
+Hello() {
     echo " this is the first calling"
     hello2
 }
-hello2(){
+hello2() {
     echo "this is the second calling"
     Hello
 }
 
-Hello(){
+Hello() {
     echo "sathya"
 }
 
-Hello
+Hellorm main
