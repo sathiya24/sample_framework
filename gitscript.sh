b4d5a96fa10d85a95d1f67456deb6856b5af2259
sathiya24 <ssnsathi125@gmail.com>
2021-02-19 16:03:57 +0530

    new change

--- gitscript.sh
+++ gitscript.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-my_branch=$(git rev-parse --abbrev-ref HEAD)
+my_branch=`git rev-parse --abbrev-ref HEAD`
 git checkout main
-git pull
-count=$(git rev-list --count $my_branch..main)
+git pull 
+count=`git rev-list --count $my_branch..main`
 echo $my_branch $count
-if [ $count -gt 0 ]; then
-   echo "upto date"
+if [ $count -gt 0 ] 
+then 
+   echo " not upto date"
 else
-   echo "not upto date"
+   echo "upto date"
 
 fi
+
--- hello.sh
+++
@@ -1,2 +0,0 @@
-echo "Line from github repository"
-echo $0 $1 $2 $3 ' > $1 $2 $3'
